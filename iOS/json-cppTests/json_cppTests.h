583c4c5907dab5cba8ae1b1853520dff672e4cb1
devmario <mario@vanillabreeze.com>
2013-02-08 19:26:11 +0900

    ios proj

---
+++ iOS/json-cppTests/json_cppTests.h
@@ -0,0 +1,13 @@
+//
+//  json_cppTests.h
+//  json-cppTests
+//
+//  Created by wonhee jang on 13. 2. 8..
+//  Copyright (c) 2013년 vanillabreeze. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface json_cppTests : SenTestCase
+
+@end
---
+++ iOS/json-cppTests/json_cppTests.m
@@ -0,0 +1,32 @@
+//
+//  json_cppTests.m
+//  json-cppTests
+//
+//  Created by wonhee jang on 13. 2. 8..
+//  Copyright (c) 2013년 vanillabreeze. All rights reserved.
+//
+
+#import "json_cppTests.h"
+
+@implementation json_cppTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in json-cppTests");
+}
+
+@end
